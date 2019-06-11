d93e555f7acde63e65e95a67ba34fdb98ec51948
Aymane SOUSSI ALAOUI <soussialaouiaymane@gmail.com>
2019-06-11 13:12:58 +0100

    added my project

---
+++ HelloJenkins.java
@@ -0,0 +1,15 @@
+package jenkinsDemo;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("dima raja "+ i);
+		}
+
+	}
+
+}
